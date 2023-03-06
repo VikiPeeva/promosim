372f5461f2369ac6f5affb72b3e207e8a3c31dbe
Viki Peeva <peeva.viki@gmail.com>
2023-03-06 12:32:19 +0100

    limiting simulated trace length

--- promosim/utils/utils.py
+++ promosim/utils/utils.py
@@ -4,7 +4,9 @@ import pm4py
 
 def extract_traces_as_strings(net, im, fm):
     play_out_log = pm4py.play_out(net, im, fm,
-                                  parameters={"add_only_if_fm_is_reached": True, "fm_leq_accepted": False})
+                                  parameters={"add_only_if_fm_is_reached": True,
+                                              "fm_leq_accepted": False,
+                                              "maxTraceLength": 10})
     traces = set([''.join(t) for t in pm4py.project_on_event_attribute(play_out_log, "concept:name")])
     return traces
 
